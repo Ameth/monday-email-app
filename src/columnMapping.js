8b2b89f3a888e796f7c3a3ecb9f8d00917de6be8
Ameth Ordoñez <amethgabriel@hotmail.com>
2024-12-04 17:30:09 -0500

    Add variables mapping file

---
+++ src/columnMapping.js
@@ -0,0 +1,17 @@
+const columnMapping = {
+  emails: {
+    to: 'mirror__1',
+    cc: 'mirror6__1',
+    bcc: 'mirror64__1',
+  },
+  subject: 'long_text__1',
+  bodyTemplate: 'texto_largo6__1',
+  variables: {
+    name: 'name',
+    file_name: 'files__1',
+    file_date: 'date4',
+    notas: 'texto9__1'
+  },
+}
+
+export default columnMapping
