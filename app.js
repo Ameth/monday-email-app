6fe39f0d5c362d9570243e5d8c4afb5b990de269
Ameth Ordoñez <amethgabriel@hotmail.com>
2024-12-29 18:41:54 -0500

    Delete files trash

--- app.js
+++
@@ -1,5 +0,0 @@
-import handleMicrosoftLogin from './src/mailer-front.js'
-
-console.log('hola')
-const loginBtn = document.getElementById('loginBtn')
-loginBtn.addEventListener('click', handleMicrosoftLogin)
